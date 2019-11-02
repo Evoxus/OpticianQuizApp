d2ce9815964662735c7bfface4b75b95c4ce8c1f
Michael Ploughman <michael@michaelhploughman.com>
2019-11-02 12:40:06 -0600

    Checked js linked correctly and tested simple view

--- index.js
+++ index.js
@@ -1,40 +1,55 @@
 const STATE = {
-    score: 0,
-    currentQuestion: QUIZ.questions[0],
+  score: 0,
+  currentQuestion: QUIZ.questions[0],
+  stage: 'NOT STARTED'
 }
 
 function startQuiz() {
-    // this will start the quiz
+  // this will start the quiz
+  // updating STATE.stage to 'QUESTIONS' from 'NOT STARTED'
+  
 }
 
-function getQuestion() {
-    // this will retreve the next question from the store
+function getQuestion(QUIZ) {
+  // this will retrieve the next question from the QUIZ object
 }
 
-function checkAnswer() {
-    // this will check the user's answer agains the correct answer determining which view goes next
+function checkAnswer(QUIZ) {
+  // this will check the user's answer against the correct answer determining which view goes next
+  if(QUIZ.questions[STATE.currentQuestion].correct === null/*userInput*/) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 function preventSkip() {
-    // this will check user has selected an input otherwise preventing moving forward
+  // this will check user has selected an input otherwise preventing moving forward
 }
 
 function displayQuestionNum() {
-    // this will get the question number displaying x of 10
+  // this will get the question number displaying x of 10
 }
 
 function rightOrWrongFeedback() {
-    // this will get the result of checkAnswer and determine the feedback to display to the user
+  // this will get the result of checkAnswer and determine the feedback to display to the user
 }
 
 function trackScore() {
-    // this will incriment score based on checkAnswer result
+  // this will increment score based on checkAnswer result
 }
 
 function displayScore() {
-    // will get score at the end of the quiz and display to user.
+  // will get score at the end of the quiz and display to user.
 }
 
-function renderQuiz() {
-    // takes quiz and state to render the proper view
+function renderQuiz(STATE, QUIZ) {
+  // takes quiz, views and state to render the proper view
+  const view = intro;
+  $('.app').html(view)
+
 }
+
+$(function(){
+  renderQuiz();
+})
---
+++ views.js
@@ -0,0 +1,3 @@
+const intro = `<h1 class="title">${QUIZ.title}</h1>
+  <button type="submit">Start?</button>`;
+
