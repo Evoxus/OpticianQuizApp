e12b564988cf6537aaacfcdaacd3ce4bb6d97171
Evoxus <michaelhploughman@gmail.com>
2019-11-02 09:32:40 -0600

    wireframed js functions, created store file

---
+++ index.js
@@ -0,0 +1,31 @@
+function startQuiz() {
+    // this will start the quiz
+}
+
+function getQuestion() {
+    // this will retreve the next question from the store
+}
+
+function checkAnswer() {
+    // this will check the user's answer agains the correct answer determining which view goes next
+}
+
+function preventSkip() {
+    // this will check user has selected an input otherwise preventing moving forward
+}
+
+function displayQuestionNum() {
+    // this will get the question number displaying x of 10
+}
+
+function rightOrWrongFeedback() {
+    // this will get the result of checkAnswer and determine the feedback to display to the user
+}
+
+function trackScore() {
+    // this will incriment score based on checkAnswer result
+}
+
+function displayScore() {
+    // will get score at the end of the quiz and display to user.
+}
